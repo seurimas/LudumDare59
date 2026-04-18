1507bd4efb44d6151dcc39fa99583a4b56d7cca2
Nicholas <nicholasanderson5@gmail.com>
2026-04-18 16:18:27 -0500

    Clean up battle stages.

--- src/rune_words/battle_states/mod.rs
+++ src/rune_words/battle_states/mod.rs
@@ -4,6 +4,21 @@ pub mod reacting;
 
 use bevy::prelude::*;
 
+use crate::dictionary::Futharkation;
+
+#[derive(Resource, Default)]
+pub struct WordBook {
+    pub words: Vec<Futharkation>,
+}
+
+#[derive(bevy::ecs::message::Message, Clone, Debug)]
+pub struct QuickTime(pub Futharkation);
+
+pub fn configure_battle_stages(app: &mut App) {
+    app.init_resource::<WordBook>();
+    app.add_message::<QuickTime>();
+}
+
 pub fn configure_battle_states(app: &mut App) {
     binding::configure_binding(app);
     acting::configure_acting(app);
--- src/rune_words/mod.rs
+++ src/rune_words/mod.rs
@@ -1,4 +1,3 @@
 pub mod battle;
-pub mod battle_stages;
 pub mod battle_states;
 pub mod rune_slots;
